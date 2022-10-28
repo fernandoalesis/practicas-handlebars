1d4c747fd75ad49ddf07f8d9f380e5b82e87217c
Fernando Gil <fernandoalesis@gmail.com>
2022-10-27 22:08:52 -0300

    Agrega seccion productos

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const app = express();
 const handlebars = require("express-handlebars");
 app.listen(8080, () => console.log("server runing in port 8080"));
 
+//middlewares para interpretar informacion recibida desde el formulario
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 //configurar motor plaSntillas
 //1-definir motor plantillas (pug y ejs no es necesario)
 app.engine("handlebars", handlebars.engine());
@@ -15,10 +19,31 @@ app.set("view engine", "handlebars");
 
 //espacio publico del servidor
 app.use(express.static("public"));
+//base datos
+const productos = [];
 //RUTAS
 app.get("/", (req, res) => {
-  res.render("home");
+  res.render("home", {
+    productos,
+  });
 });
 app.get("/contacto", (req, res) => {
   res.render("contact");
+});
+app.get("/usuarios", (req, res) => {
+  res.render("usuarios", {
+    estudiantes: [{ name: "pedro" }, { name: " maria" }, { name: "carlos" }],
+  });
+});
+
+app.get("/productos", (req, res) => {
+  res.render("productos", {
+    productos,
+  });
+});
+app.post("/productos", (req, res) => {
+  const newProduct = req.body;
+  productos.push(newProduct);
+  console.log(productos);
+  res.redirect("/productos");
 });
